f9e0db07c287f120a475347370349c3085d158a6
PremiumDelevent <jofre.coll@premiumdelevent.com>
2025-09-29 10:12:51 +0200

    :bug: BUG: keep db status on every sync

--- backend/infrastructure/database/pg/PgOrdersRepository.js
+++ backend/infrastructure/database/pg/PgOrdersRepository.js
@@ -29,12 +29,11 @@ class PgOrdersRepository extends OrdersRepository {
 
     async save(order) {
         await this.pool.query(
-            `INSERT INTO orders (num, sellto_customer_name, furniture_load_date_jmt, jmt_status, jmtEventName, lineas, updated_at)
-            VALUES ($1, $2, $3, $4, $5, $6, now())
+            `INSERT INTO orders (num, sellto_customer_name, furniture_load_date_jmt, jmtEventName, lineas, updated_at)
+            VALUES ($1, $2, $3, $4, $5, now())
             ON CONFLICT (num)
             DO UPDATE SET sellto_customer_name = EXCLUDED.sellto_customer_name,
                             furniture_load_date_jmt = EXCLUDED.furniture_load_date_jmt,
-                            jmt_status = EXCLUDED.jmt_status,
                             jmtEventName = EXCLUDED.jmtEventName,
                             lineas = EXCLUDED.lineas,
                             updated_at = now()`,
@@ -42,7 +41,6 @@ class PgOrdersRepository extends OrdersRepository {
                 order.num,
                 order.sellto_customer_name,
                 order.furniture_load_date_jmt,
-                order.jmt_status,
                 order.jmteventname,
                 JSON.stringify(order.lineas)
             ]
